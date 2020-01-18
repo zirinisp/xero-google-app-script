a8a48c0686409dc08ce6d50b8efe8dd28930ceed
Pantelis Zirinis <info@paz-labs.com>
2020-01-18 11:45:14 +0000

    Added all files

---
+++ XeroService.js
@@ -0,0 +1,132 @@
+// https://github.com/gsuitedevs/apps-script-oauth2
+function getXeroService() {
+  // Create a new service with the given name. The name will be used when
+  // persisting the authorized token, so ensure it is unique within the
+  // scope of the property store.
+  return OAuth2.createService("xero")
+  
+  // Set the endpoint URLs.
+  .setAuthorizationBaseUrl('https://login.xero.com/identity/connect/authorize')
+  .setTokenUrl('https://identity.xero.com/connect/token')
+  
+  // Set the client ID and secret
+  .setClientId('9175F8AFA3AD439E9E72DB9D1762D89D')
+  .setClientSecret('gr1tWCDmMqMIiy7krTO-hV47D2b4TTpHtd1THr2go33uVbGi')
+  
+  // Set the name of the callback function in the script referenced
+  // above that should be invoked to complete the OAuth flow.
+  .setCallbackFunction('authCallback')
+  
+  // Set the property store where authorized tokens should be persisted.
+  .setPropertyStore(PropertiesService.getDocumentProperties())
+  
+  // Set the scopes to request
+  .setScope('accounting.transactions.read offline_access')
+  
+  // Requests offline access.
+  .setParam('access_type', 'offline')
+  
+  // xero specific
+  .setParam('redirect_uri','https://script.google.com/macros/d/1YXcJ3q9Uq9sj4jooJDsWKG4mSdO09FtbhldtPQ5_e7Wqw40VbwuNzQdE/usercallback')
+  .setParam('client_id','9175F8AFA3AD439E9E72DB9D1762D89D')
+  .setParam('response_type','code')  
+  
+}
+
+function logout(){
+  var service = getXeroService();
+  service.reset();
+}
+
+function authCallback(request) {
+  var xeroService = getXeroService();
+  var isAuthorized = xeroService.handleCallback(request);
+  if (isAuthorized) {
+    return HtmlService.createHtmlOutput('Success! You can close this tab.');
+  } else {
+    return HtmlService.createHtmlOutput('Denied. You can close this tab');
+  }
+}
+
+function getTenantId() {
+  var user_props = PropertiesService.getDocumentProperties(),
+      XUPK = "oauth2.xero", // Xero User Properties Key
+      tenantId;  
+  var oauth2_xero = JSON.parse(user_props.getProperty(XUPK));
+  
+  // return if tenantId already saved for more information on tenant Ids visit https://developer.xero.com/documentation/oauth2/auth-flow
+  if(oauth2_xero["tenantId"] && oauth2_xero["tenantId"] != "") return oauth2_xero["tenantId"];
+    
+  var xeroService = getXeroService();
+  var response = UrlFetchApp.fetch('https://api.xero.com/connections', {
+    headers: {
+      'Authorization': 'Bearer ' + xeroService.getAccessToken(),
+      'Content-Type': 'application/json'
+    }
+  });
+  
+  tenantId = JSON.parse(response.getContentText())[0]["tenantId"];
+  oauth2_xero["tenantId"] = tenantId;
+  user_props.setProperty(XUPK, JSON.stringify(oauth2_xero));
+  
+  return tenantId;  
+}
+
+/**
+ * Converts key value pair to percent encoded param string
+ * @param {object} obj String object
+ */
+String.prototype.addQuery = function(obj,ignore_keys) {
+  return this + Object.keys(obj).reduce(function(p, e, i) {
+    return p + (i == 0 ? "?" : "&") +
+      (Array.isArray(obj[e]) ? obj[e].reduce(function(str, f, j) {
+        return str + e + "=" + encodeURIComponent(f) + (j != obj[e].length - 1 ? "&" : "")
+      },"") : e + "=" + (Array.isArray(ignore_keys) ? (ignore_keys.indexOf(e) === -1 ? encodeURIComponent(obj[e]) : obj[e]) : encodeURIComponent(obj[e])));
+  },"");
+}
+
+function getXeroHeaders(){
+    return {
+        'Authorization': 'Bearer ' + getXeroService().getAccessToken(),
+        'Content-Type': 'application/json',
+        'Accept' : 'application/json',
+        'Xero-tenant-id' : getTenantId()
+    } 
+}
+
+/**
+ * Gets the invoices according to a where clause
+ * @param {number} page An integer
+ * @param {string} filter A valid where clause for more information visit https://developer.xero.com/documentation/api/requests-and-responses#get-modified
+ */
+function getXeroInvoices(complexQuery){   
+  var url = "https://api.xero.com/api.xro/2.0/Invoices";
+  
+  // see addQuery prototype for details
+  var end_point = url.addQuery(complexQuery,["Statuses"]);
+  
+  var response = UrlFetchApp.fetch(end_point, {
+    headers : getXeroHeaders()
+  })
+
+  return JSON.parse(response.getContentText()).Invoices;
+}
+
+function logauthURL(){
+  var xeroService = getXeroService();
+  if (!xeroService.hasAccess())
+    Logger.log(xeroService.getAuthorizationUrl());
+}
+
+function logUserProperties(){
+  Logger.log(JSON.stringify(PropertiesService.getDocumentProperties().getProperties()));
+}
+
+function getTenantIdTest(){
+  Logger.log(getTenantId());
+}
+
+function getXeroInvoiceTest(){
+  Logger.log(getXeroInvoices(1,"Date >= DateTime(2019, 12, 01) && Date < DateTime(2019, 12, 31)")); // year month date
+  // Statuses=PAID
+}
